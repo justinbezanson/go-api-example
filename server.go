f242e576fb8ff3477211b0681740f8d533a0eb3d
Justin Bezanson <justinbezanson@gmail.com>
2025-06-17 20:43:04 -0300

    refactor + post requests

--- server.go
+++ server.go
@@ -1,7 +1,11 @@
 package main
 
-import "net/http"
-import "encoding/json"
+import (
+	"net/http"
+	"encoding/json"
+	"sync"
+	"io/ioutil"
+)
 
 type Coaster struct {
 	Name string `json:"name"`
@@ -11,13 +15,14 @@ type Coaster struct {
 	Height int `json:"height"`
 }
 
-type coastersHandler struct {
-	store map[string]Coaster
+type coastersController struct {
+	sync.Mutex
+	data map[string]Coaster
 }
 
-func newCoasterHandler() *coastersHandler {
-	return &coastersHandler{
-		store: map[string]Coaster{
+func newCoasterController() *coastersController {
+	return &coastersController{
+		data: map[string]Coaster{
 			"1": {
 				Name: "Roller Coaster 1", 
 				Manufacturer: "Coaster Inc.", 
@@ -29,13 +34,57 @@ func newCoasterHandler() *coastersHandler {
 	}
 }
 
-func (h *coastersHandler) get(w http.ResponseWriter, r *http.Request) {
-	coasters := make([]Coaster, len(h.store))
+func (h *coastersController) coasters(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.index(w, r)
+		return
+	case http.MethodPost:
+		h.store(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed\n"))
+		return
+	}
+}
+
+func (h *coastersController) store(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	
+	var coaster Coaster
+	err = json.Unmarshal(bodyBytes, &coaster)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid JSON format\n"))
+		return
+	}
+	
+	h.Lock()
+	h.data[coaster.ID] = coaster
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Coaster created successfully\n"))
+	defer h.Unlock()
+
+
+}
+
+func (h *coastersController) index(w http.ResponseWriter, r *http.Request) {
+	coasters := make([]Coaster, len(h.data))
+
+	h.Lock()
 	i := 0
-	for _, coaster := range h.store {
+	for _, coaster := range h.data {
 		coasters[i] = coaster
 		i++
 	}
+	h.Unlock()
 
 	jsonBtyes, err := json.Marshal(coasters)
 	if err != nil {
@@ -49,9 +98,9 @@ func (h *coastersHandler) get(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	coastersHandler := newCoasterHandler()
+	coasterController := newCoasterController();
 
-	http.HandleFunc("/coasters", coastersHandler.get)
+	http.HandleFunc("/coasters", coasterController.coasters)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
